133ff99d97be56c4c006aa1aae66ff4609f34a20
Bin Gao <bingao@g.harvard.edu>
2015-09-19 21:17:33 -0400

    add run(dirname, filename)

--- runme.py
+++ runme.py
@@ -1,19 +1,34 @@
 import sys
 import os
 
-def run(file):
-  print (file)
+BASE_COUNT = 50
+
+def run(dirname, file):
+  f = open(dirname + '/' + file)
+  entries = []
+
+  for i in range(50):
+    entry = f.readline().strip()
+    entries.append(entry)
+    
+  print (file, entries)
 
 def main(argv):
-  if len(argv) != 1:
-    print ('runme.py <dirname>')
+  len_argv = len(argv)
+  if len_argv != 1 and len_argv != 2:
+    print ('runme.py <dirname> [filename]')
     sys.exit()
 
   dirname = argv[0]
 
-  for file in os.listdir("./" + dirname):
-    if file.endswith(".txt"):
-      run(file)
+  if len_argv == 1:
+    for file in os.listdir("./" + dirname):
+      if file.endswith(".txt"):
+        run(dirname, file)
+  else:
+    assert len_argv == 2
+    filename = argv[1]
+    run(dirname, filename)
 
 if __name__ == "__main__":
   main(sys.argv[1:])
