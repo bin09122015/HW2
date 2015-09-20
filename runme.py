df278097e3471ab057d554093ea7a94c4c1b94e6
Bin Gao <bingao@g.harvard.edu>
2015-09-19 20:48:22 -0400

    tab to 2 spaces

--- runme.py
+++ runme.py
@@ -2,19 +2,19 @@ import sys
 import os
 
 def run(file):
-	print (file)
+  print (file)
 
 def main(argv):
-	if len(argv) != 1:
-		print ('runme.py <dirname>')
-		sys.exit()
+  if len(argv) != 1:
+    print ('runme.py <dirname>')
+    sys.exit()
 
-	dirname = argv[0]
+  dirname = argv[0]
 
-	for file in os.listdir("./" + dirname):
-		if file.endswith(".txt"):
-			run(file)
+  for file in os.listdir("./" + dirname):
+    if file.endswith(".txt"):
+      run(file)
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+  main(sys.argv[1:])
 
