0d3db42014509eda9d96d8f2e04b5fadf5070473
Bin Gao <bingao@g.harvard.edu>
2015-09-27 19:06:29 -0400

    merge attempt

--- runme.py
+++ runme.py
@@ -20,18 +20,36 @@ def get_confidence_interval(data, confidence=0.95):
 
   return m-h, m+h
 
-def detect_change(base, target):
+def detect_mean_change(base, target):
   (left, right) = get_confidence_interval(base)
   target_mean = sum(target)/len(target)
 
-  # print ("(" + str(left) + "," + str(right) + ")", str(target_mean))
-  (mean_cntr, var_cntr, std_cntr) = sp.stats.bayes_mvs(base, alpha=0.95)
-  target_variance = (np.std(target, ddof=1))**2
-  print ("(" + str(var_cntr[1][0]) + "," + str(var_cntr[1][1]) + ")", str(target_variance))
-
-  return target_variance < var_cntr[1][0] or target_variance > var_cntr[1][1]
+  # print ("mean: (" + str(left) + "," + str(right) + ")", str(target_mean))
 
   return target_mean < left or target_mean > right
+
+def reject_outliers(data):
+  return data
+
+  tmp = data[:]
+  tmp_max = max(tmp)
+  tmp_min = min(tmp)
+  tmp.remove(tmp_max)
+  tmp.remove(tmp_min)
+
+  assert len(tmp) == len(data)-2
+
+  return tmp
+
+def detect_var_change(base, target):
+  base_minus_outliers = reject_outliers(base)
+  (mean_cntr, var_cntr, std_cntr) = sp.stats.bayes_mvs(base_minus_outliers, alpha=0.95)
+  target_minus_outliers = reject_outliers(target)
+  target_variance = (np.std(target_minus_outliers, ddof=1))**2
+
+  print ("variance: (" + str(var_cntr[1][0]) + "," + str(var_cntr[1][1]) + ")", str(target_variance))
+
+  return target_variance < var_cntr[1][0] or target_variance > var_cntr[1][1]
 
 def get_next_entry(f):
   entry = f.readline().strip()
@@ -51,7 +69,7 @@ def move_windows(base, target, starting, new_entry):
     # print (base)
 
   slide_window(target, new_entry)
-  print (target)
+  # print (target)
 
   starting += 1
 
@@ -73,7 +91,8 @@ def run(dirname, file):
     base_window = tmp_entries[-WINDOW_LEN:]
     target_window = tmp_entries[-WINDOW_LEN:]
     starting_location = 0
-    start_change = -1
+    start_mean_change = -1
+    start_var_change = -1
 
     while True:
       new_entry = get_next_entry(f)
@@ -84,19 +103,35 @@ def run(dirname, file):
                                        target_window,
                                        starting_location,
                                        new_entry)
-      if detect_change(base_window, target_window):
-        if start_change == -1:
-          start_change = starting_location + WINDOW_LEN - 1
-        if starting_location >= start_change:
-          print (start_change, starting_location)
+      if detect_mean_change(base_window, target_window):
+        if start_mean_change == -1:
+          start_mean_change = starting_location + WINDOW_LEN - 1
+        if starting_location >= start_mean_change:
+          print ("mean_b", start_mean_change, starting_location)
+          start_mean_change = -1
           break
-        print (start_change, starting_location)
+        # print ("mean", start_mean_change, starting_location)
       else:
-        if start_change != -1:
-          start_change = -1
+        if start_mean_change != -1:
+          start_mean_change = -1
+
+      if detect_var_change(base_window, target_window):
+        if start_var_change == -1:
+          start_var_change = starting_location + WINDOW_LEN - 1
+        if start_mean_change == -1 and starting_location >= start_var_change:
+          print ("var_b", start_var_change, starting_location)
+          start_var_change = -1
+          break
+        print ("var", start_var_change, starting_location)
+      else:
+        if start_var_change != -1:
+          start_var_change = -1
     
-    if start_change != -1 and (starting_location-start_change) < WINDOW_LEN/2:
-        print (start_change, starting_location)
+    if start_mean_change != -1 and (start_mean_change-starting_location) < WINDOW_LEN/2:
+        print ("mean_e", start_mean_change, starting_location)
+
+    if start_var_change != -1 and (start_var_change-starting_location) < WINDOW_LEN/2:
+        print ("var_e", start_var_change, starting_location)
   else:
     print ("Categorical data should go here")
 
